10c61fe4afd1343659191c3e7d83237c7ec46201
Amadeus-911 <iftikharimrul@iut-dhaka.edu>
2023-06-11 05:32:04 +0600

    demo

--- src/GoogleClient.php
+++ src/GoogleClient.php
@@ -6,16 +6,21 @@ class GoogleClient
 {
 
     private $client_id;
+    private $client_secret;
     private $redirect_uri;
     private $scope;
     private $response_type = 'code';
+    private $grant_type = 'authorization_code';
 
-    public $apiUrl = 'https://accounts.google.com/o/oauth2/v2/auth';
+    private $apiUrl = 'https://accounts.google.com/o/oauth2/v2/auth';
+    private $tokenUrl = 'https://accounts.google.com/o/oauth2/token';
+    private $userInfoUrl = 'https://www.googleapis.com/oauth2/v3/userinfo';
 
-    public function __construct($client_id, $redirect_uri, $scope) {
+    public function __construct($client_id, $client_secret, $redirect_uri, $scope) {
         $this->client_id = $client_id;
         $this->redirect_uri = $redirect_uri;
         $this->scope = $scope;
+        $this->client_secret = $client_secret;
     }
 
       // Build the authorization URL with the parameter values
@@ -29,38 +34,82 @@ class GoogleClient
         return $authUrl;
     }
 
-   // Initialize cURL session
-    public function initCurlSession($authUrl){
+    public function getAccessToken($code){
         $ch = curl_init();
 
-        // Set cURL options
-        curl_setopt($ch, CURLOPT_URL, $authUrl);
+        // Set the URL and other options
+        curl_setopt($ch, CURLOPT_URL, $this->tokenUrl);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        return $ch;
-    }
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, 'code=' . urlencode($code) . 
+        '&client_id=' . urlencode($this->client_id) . 
+        '&client_secret=' . urlencode($this->client_secret) . 
+        '&redirect_uri=' . urlencode($this->redirect_uri) . 
+        '&grant_type=' . urlencode($this->grant_type));
     
-        // Execute the cURL request
-    public function executeRequestForCode($ch){
-        
+        // Execute the request and fetch the response
         $response = curl_exec($ch);
-
+    
         // Check for errors
         if (curl_errno($ch)) {
             $error = curl_error($ch);
-            // Handle the error as needed
-            echo "cURL Error: " . $error;
         }
     
         // Close the cURL resource
         curl_close($ch);
     
         // Handle the response
-        // if ($response) {
-        // ;
-        // } else {
-    
-        // }
+        if ($response) {
+            // Process the response as needed
+            $jsonObj = json_decode($response);
+            $accessToken = $jsonObj->access_token;
+
+            return $accessToken;
+        } else {
+            return "Error";
+        }
+    }
+
+    public function getUserInfo($accessToken){
+
+
+        // Initialize cURL session
+        $ch = curl_init();
+
+        // Set cURL options
+        curl_setopt($ch, CURLOPT_URL, $this->userInfoUrl);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Authorization: Bearer ' . $accessToken
+        ));
+
+        // Execute the cURL request
+        $response = curl_exec($ch);
+
+        // Check for errors
+        if (curl_errno($ch)) {
+            $error = curl_error($ch);
+        }
+
+        // Close the cURL resource
+        curl_close($ch);
+
+        // Handle the response
+        if ($response) {
+            // Process the response as needed
+            $data = json_decode($response);
+            // Access the data
+            $email = $data->email;
+            $imageUrl = $data->picture;
+            $name = $data->family_name ? $data->given_name . ' ' . $data->family_name : $data->given_name;
+
+            $user = [$name, $email, $imageUrl];
+
+            return $user;
+
+        } else {
+            return "error1 ";
+        }
     }
 
     
